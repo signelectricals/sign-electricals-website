252b5eaa009b33e8fb78e7c36d44399ff181d3e1
signelectricals <sauravdutta021@gmail.com>
2026-07-14 04:13:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -139,3 +139,18 @@ if(year){
     year.textContent=new Date().getFullYear();
 
 }
+/* ================= FAQ ================= */
+
+const faqItems = document.querySelectorAll(".faq-item");
+
+faqItems.forEach(item=>{
+
+    const question=item.querySelector(".faq-question");
+
+    question.addEventListener("click",()=>{
+
+        item.classList.toggle("active");
+
+    });
+
+});
