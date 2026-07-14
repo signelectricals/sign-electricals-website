8bbad1ff0a365268b51c84448a0c1925c3b91d50
signelectricals <sauravdutta021@gmail.com>
2026-07-14 18:57:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -32,3 +32,22 @@ if (slides.length > 0) {
     setInterval(nextSlide, 3000);
 
 }
+// ==========================
+// NAVBAR SHADOW
+// ==========================
+
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+
+    if (header) {
+
+        if (window.scrollY > 50) {
+            header.style.boxShadow = "0 10px 30px rgba(0,0,0,.20)";
+        } else {
+            header.style.boxShadow = "0 5px 15px rgba(0,0,0,.08)";
+        }
+
+    }
+
+});
