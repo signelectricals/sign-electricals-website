16cf564db6edf517e8904c99a23bf84c609ef7fb
signelectricals <sauravdutta021@gmail.com>
2026-07-14 18:59:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -51,3 +51,35 @@ window.addEventListener("scroll", () => {
     }
 
 });
+// ==========================
+// ACTIVE MENU
+// ==========================
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-links a");
+
+window.addEventListener("scroll", () => {
+
+    let current = "";
+
+    sections.forEach(section => {
+
+        const sectionTop = section.offsetTop - 150;
+
+        if (window.scrollY >= sectionTop) {
+            current = section.getAttribute("id");
+        }
+
+    });
+
+    navLinks.forEach(link => {
+
+        link.classList.remove("active");
+
+        if (link.getAttribute("href") === "#" + current) {
+            link.classList.add("active");
+        }
+
+    });
+
+});
